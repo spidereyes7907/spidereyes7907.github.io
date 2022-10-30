fd751b21f5f3b39e71059f7ceddc3c18341b8268
spidereyes7907 <117020634+spidereyes7907@users.noreply.github.com>
2022-10-30 13:04:59 +0000

    Add files via upload

---
+++ website/New folder/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double num01;
+double num02;
+double num03;
+
+Console.Write("Input a number: ");
+
+num01 = Convert.ToDouble( Console.ReadLine() );
+
+Console.Write("input a second number");
+
+num02 = Convert.ToDouble( Console.ReadLine() );
+
+Console.WriteLine("input a third number");
+
+num03 = Convert.ToDouble( Console.ReadLine());
+
+double result = (num01 + num02 + num03) /3 ;
+
+Console.WriteLine("the result is " + result);
+
+
+Console.ReadKey();
+
